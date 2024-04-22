ae97efb9946978add148e7910ca590e4b643bac4
Kevin Kener <kevink.contacto@gmail.com>
2024-04-22 13:48:28 -0300

    add config for .md files

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	// output: 'export',
-	distDir: 'build'
+	distDir: 'build',
+	webpack: (config) => {
+		config.module.rules.push(
+		  {
+			test: /\.md$/,
+			use: 'raw-loader'
+		  }
+		)
+	
+		return config
+	  }
 }
 
 module.exports = nextConfig
