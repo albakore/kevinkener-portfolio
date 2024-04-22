b8708fca29a83c25acb55e54ffdfa5b5c0d6facc
Kevin Kener <kevink.contacto@gmail.com>
2024-04-22 13:52:26 -0300

    seccion descripcion ahora admite archivo .md

--- src/sections/Descripcion.jsx
+++ src/sections/Descripcion.jsx
@@ -1,13 +1,30 @@
+'use client'
 import SeccionPagina from "@/components/SeccionPagina";
 import React from "react";
 import { Text, Flex, Container, VStack, HStack, Button, Box, useBreakpointValue } from "@chakra-ui/react";
 import { GetCV } from "@/contexts/Portfolio";
 import Markdown from 'react-markdown'
-
+import remarkGfm from 'remark-gfm'
+import descripcion from '../contexts/descripcion.md'
 
 export default function Descripcion({ ref }) {
 	const movil = useBreakpointValue({ base: true, md: false })
-	const { descripcion } = GetCV()
+	const [documento, setDocumento] = React.useState(null)
+
+	
+	if (!documento) {
+		return (
+			<SeccionPagina id='descripcion' noFullHeight={!movil} withScrollY={movil}>
+			{/* <Text fontSize={"3xl"} mb={5}>Sobre Mi</Text> */}
+				<Container maxW={'5xl'}>
+					<Flex ref={ref} maxW={'90vw'} direction={'column'} gap={5}  >
+						<Markdown remarkPlugins={[remarkGfm]}>{descripcion}</Markdown>
+					</Flex>
+				</Container>
+			</SeccionPagina>
+		)
+	}
+	
 	return (
 		<SeccionPagina id='descripcion' noFullHeight={!movil} withScrollY={movil}>
 			{/* <Text fontSize={"3xl"} mb={5}>Sobre Mi</Text> */}
@@ -15,9 +32,7 @@ export default function Descripcion({ ref }) {
 				<Flex ref={ref} maxW={'90vw'} direction={'column'} gap={5}  >
 					
 					<Text fontSize={'xl'}>
-					<Markdown >
-						{descripcion}
-					</Markdown>
+						<Markdown remarkPlugins={[remarkGfm]}>{descripcion}</Markdown>
 					</Text>
 
 				</Flex>
